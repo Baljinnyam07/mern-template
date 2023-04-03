f1d683ce7b7f4d279fe7a7dbf3069e01b419c63e
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-04-03 18:00:27 +0800

    commit

---
+++ typescript/app.ts
@@ -0,0 +1,17 @@
+import express, {Express, Request, Response} from "express";
+import usersRouter from "./routes/usersRouter";
+
+const app:Express = express();
+
+app.get('/', (req:Request, res:Response) => {
+    res.json('Hello World');
+});
+
+app.get("/hello", (req:Request, res:Response)=>{
+    res.json("hi")
+})
+
+app.use("/api/users",usersRouter);
+
+export default app;
+
---
+++ typescript/controllers/usersController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express"
+import UserModel, { UserInterface } from "../models/userModel"
+
+export const countAllUsers = async (req:Request, res:Response)=>{
+    res.json(await UserModel.count({}))
+}
+
+export const findAllUsers = async (req:Request, res:Response) =>{
+    const { limit = "10", skip = "0" } = req.query;
+    const result: UserInterface[] = await UserModel.find({}).limit(Number(limit)).skip(Number(skip));
+    res.json(result);
+};
+
+export const findUserById = async (req:Request, res:Response) => {
+    const { _id } = req.params;
+    const result:UserInterface | null = await UserModel.findById(_id);
+    res.json(result)
+};
+
+export const createUser = async (req:Request, res:Response) =>{
+    const newUser = await UserModel.create(req.body);
+    res.json(newUser)
+};
+
+export const deleteUser = async (req:Request, res:Response) =>{
+    const { _id } = req.params
+    const result = await UserModel.findOneAndDelete({_id});
+    res.json(result)
+};
+
+export const updateUser = async (req:Request, res:Response) =>{
+    const { _id } = req.params
+    const user = req.body
+    const result = await UserModel.findOneAndUpdate({_id}, user);
+    res.json(result)
+};
